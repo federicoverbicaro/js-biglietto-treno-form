4e203d37f3c3bdf612aedd9e81e10384a1e7ac67
federicoverbicaro <federico.verbicaro@icloud.com>
2024-01-08 20:28:30 +0100

    feat:fine Esercizio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 const button = document.getElementById("bottone")
+const buttonAnnulla = document.getElementById("bottoneAnnulla");
 
 button.addEventListener("click", function () {
 
@@ -6,8 +7,16 @@ button.addEventListener("click", function () {
 
     let kilometri = parseFloat(document.getElementById("kilometri").value)
     let anniUtente = parseInt(document.getElementById("eta").value)
+
+
     let totaleRisultato = document.getElementById("totaleRisultato")
-    
+
+    let carozza = document.getElementById("carozzaAssegnata")
+    carozza = Math.floor((Math.random() * (0, 8 + 1) + 0))
+
+    let codice = document.getElementById("codiceCp")
+    codice = Math.floor((Math.random() * (10000, 100000 + 0) + 0))
+
 
 
     const tariffaKilometro = 0.21
@@ -28,5 +37,17 @@ button.addEventListener("click", function () {
 
     }
 
+
     document.getElementById("risultatoNomeOutput").textContent = nome
+
+    document.getElementById("carozzaAssegnata").textContent = carozza
+
+    document.getElementById("codiceCp").textContent = codice
 })
+
+buttonAnnulla.addEventListener("click", function () {
+    
+    document.getElementById("risultatoNome").value = "";
+    document.getElementById("kilometri").value = "";
+    document.getElementById("eta").value = "";
+});
