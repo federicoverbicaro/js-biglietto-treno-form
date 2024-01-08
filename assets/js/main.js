bbf14e7d8144233a0816c59afea26921b1dee864
federicoverbicaro <federico.verbicaro@icloud.com>
2024-01-08 17:29:25 +0100

    feat:Prima parte

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+const button = document.getElementById("bottone")
+
+button.addEventListener("click", function () {
+
+    let kilometri = parseFloat(document.getElementById("kilometri").value)
+    let anniUtente = parseInt(document.getElementById("eta").value)
+    let totaleRisultato = document.getElementById("totaleRisultato")
+
+
+
+    const tariffaKilometro = 0.21
+    const totale = kilometri * tariffaKilometro
+
+    if (anniUtente < 18) {
+        totaleRisultato.textContent = `${((totale * 0.8).toFixed(2))}€`
+        console.log(totale)
+
+    } else if (anniUtente >= 64) {
+
+        totaleRisultato.textContent = `${((totale * 0.6).toFixed(2))}€`
+        console.log(totale)
+    } else {
+
+        totaleRisultato.textContent = `${((totale).toFixed(2))}€`
+        console.log(totale)
+
+    }
+})
